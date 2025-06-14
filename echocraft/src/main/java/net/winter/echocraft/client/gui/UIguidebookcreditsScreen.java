cfd2e6e53f5755d7476b00fea473d5c58b6e90ca
Dane <113046060+WinterSpark1@users.noreply.github.com>
2025-06-14 13:42:13 -0600

    Reorganized Guide Book Files

--- echocraft/src/main/java/net/winter/echocraft/client/gui/UIguidebookcreditsScreen.java
+++ echocraft/src/main/java/net/winter/echocraft/client/gui/UIguidebookcreditsScreen.java
@@ -8,6 +8,7 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.chat.Component;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.GuiGraphics;
 
 import java.util.HashMap;
@@ -19,6 +20,8 @@ public class UIguidebookcreditsScreen extends AbstractContainerScreen<UIguideboo
 	private final Level world;
 	private final int x, y, z;
 	private final Player entity;
+	Button button_prev;
+	Button button_next;
 
 	public UIguidebookcreditsScreen(UIguidebookcreditsMenu container, Inventory inventory, Component text) {
 		super(container, inventory, text);
@@ -27,7 +30,7 @@ public class UIguidebookcreditsScreen extends AbstractContainerScreen<UIguideboo
 		this.y = container.y;
 		this.z = container.z;
 		this.entity = container.entity;
-		this.imageWidth = 176;
+		this.imageWidth = 256;
 		this.imageHeight = 166;
 	}
 
@@ -37,7 +40,7 @@ public class UIguidebookcreditsScreen extends AbstractContainerScreen<UIguideboo
 	public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
 		super.render(guiGraphics, mouseX, mouseY, partialTicks);
 		this.renderTooltip(guiGraphics, mouseX, mouseY);
-		if (mouseX > leftPos + 6 && mouseX < leftPos + 30 && mouseY > topPos + 34 && mouseY < topPos + 58) {
+		if (mouseX > leftPos + 7 && mouseX < leftPos + 31 && mouseY > topPos + 22 && mouseY < topPos + 46) {
 			guiGraphics.renderTooltip(font, Component.translatable("gui.echocraft.u_iguidebookcredits.tooltip_crea"), mouseX, mouseY);
 		}
 	}
@@ -49,7 +52,9 @@ public class UIguidebookcreditsScreen extends AbstractContainerScreen<UIguideboo
 		RenderSystem.defaultBlendFunc();
 		guiGraphics.blit(texture, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight, this.imageWidth, this.imageHeight);
 
-		guiGraphics.blit(ResourceLocation.parse("echocraft:textures/screens/winter2.png"), this.leftPos + 0, this.topPos + 99, 0, 0, 64, 64, 64, 64);
+		guiGraphics.blit(ResourceLocation.parse("echocraft:textures/screens/winter2.png"), this.leftPos + 4, this.topPos + 99, 0, 0, 64, 64, 64, 64);
+
+		guiGraphics.blit(ResourceLocation.parse("echocraft:textures/screens/guide_book.png"), this.leftPos + 11, this.topPos + 26, 0, 0, 16, 16, 16, 16);
 
 		RenderSystem.disableBlend();
 	}
@@ -65,12 +70,20 @@ public class UIguidebookcreditsScreen extends AbstractContainerScreen<UIguideboo
 
 	@Override
 	protected void renderLabels(GuiGraphics guiGraphics, int mouseX, int mouseY) {
-		guiGraphics.drawString(this.font, Component.translatable("gui.echocraft.u_iguidebookcredits.label_winterspark"), 6, 8, -12829636, false);
-		guiGraphics.drawString(this.font, Component.translatable("gui.echocraft.u_iguidebookcredits.label_programmer"), 6, 21, -12829636, false);
+		guiGraphics.drawString(this.font, Component.translatable("gui.echocraft.u_iguidebookcredits.label_winterspark"), 192, 128, -12829636, false);
+		guiGraphics.drawString(this.font, Component.translatable("gui.echocraft.u_iguidebookcredits.label_programmer"), 5, 6, -12829636, false);
 	}
 
 	@Override
 	public void init() {
 		super.init();
+		button_prev = Button.builder(Component.translatable("gui.echocraft.u_iguidebookcredits.button_prev"), e -> {
+		}).bounds(this.leftPos + 157, this.topPos + 141, 46, 20).build();
+		guistate.put("button:button_prev", button_prev);
+		this.addRenderableWidget(button_prev);
+		button_next = Button.builder(Component.translatable("gui.echocraft.u_iguidebookcredits.button_next"), e -> {
+		}).bounds(this.leftPos + 205, this.topPos + 141, 46, 20).build();
+		guistate.put("button:button_next", button_next);
+		this.addRenderableWidget(button_next);
 	}
 }
